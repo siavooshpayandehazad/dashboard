fa0f652047e291fea5d42a4dea20ef1e24298466
siavooshpayandehazad <siavooshpayandeh@gmail.com>
2022-05-27 20:20:51 +0200

    add edit and delete option to finance lists

--- page_classes/finances.py
+++ page_classes/finances.py
@@ -51,11 +51,45 @@ class Finances(Resource):
         today_date = parse_date(args['date'])
         if args['tracker_type'] == 'saving':
             return add_saving_item_to_table(args['value'].lower(), today_date, self.c, self.conn, self.lock)
+
         if args['tracker_type'] == 'mortgage':
             return add_mortgage_item_to_table(args['value'].lower(), today_date, self.c, self.conn, self.lock)
 
         if args['tracker_type'] == 'spending':
             item_value = [x.strip() for x in args['value'].lower().split(",")]
-            add_data_to_finance_db(self.c_finance, self.conn_finance, today_date, item_value[0], item_value[1],
-                                   item_value[2], self.lock)
+            add_data_to_finance_db(self.c_finance, self.conn_finance, today_date, item_value[0].strip(),
+                                   item_value[1].strip(), item_value[2].strip(), self.lock)
+
+        if args['action'] == "update":
+            old_vals = eval(args['oldValue'])
+            old_date = old_vals["date"].strip()
+            old_name = old_vals["name"].strip()
+            old_cost = old_vals["cost"].strip()
+            old_type = old_vals["type"].strip()
+
+            new_vals = eval(args['value'])
+            new_date = new_vals["date"].strip()
+            new_name = new_vals["name"].strip()
+            new_cost = new_vals["cost"].strip()
+            new_type = new_vals["type"].strip()
+
+            self.lock.acquire(True)
+            self.c_finance.execute("UPDATE finance "
+                                   "SET date = ? , name = ? , cost = ? , type = ? "
+                                   "WHERE date = ? AND name = ? AND cost = ? AND type = ?",
+                                   (new_date, new_name, new_cost, new_type, old_date, old_name, old_cost, old_type))
+            self.conn_finance.commit()
+            self.lock.release()
+
+        if args['action'] == "delete":
+            vals = eval(args['value'])
+            date = vals["date"].strip()
+            name = vals["name"].strip()
+            cost = vals["cost"].strip()
+            type = vals["type"].strip()
+            self.lock.acquire(True)
+            self.c_finance.execute("""DELETE from finance WHERE date = ? AND name = ? AND cost = ? AND type = ?""",
+                                   (date, name, cost, type))
+            self.conn_finance.commit()
+            self.lock.release()
         return "Done", 200
